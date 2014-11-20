d2fbd6956b7af148dbf310c9e9dd8c3e45ae8b57
Trisha Gee <trishagee@github.com>
2014-11-20 13:31:03 +0100

    [JAVA-1390] Phase 2: doing the release. Upload Archives is working

--- buildSrc/src/main/groovy/com/mongodb/release/ReleasePlugin.groovy
+++ buildSrc/src/main/groovy/com/mongodb/release/ReleasePlugin.groovy
@@ -2,7 +2,6 @@ package com.mongodb.release
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.maven.MavenDeployment
 
 /**
  * Inspired originally by this <a href="https://github.com/trnl/github-release-gradle-plugin/">Release Plugin</a>.  This co-ordinates the
@@ -16,6 +15,17 @@ import org.gradle.api.artifacts.maven.MavenDeployment
  * As such, you need to provide the configuration those tools need.  You'll need Nexus and Github credentials in 
  * <code>~/.gradle/gradle.properties</code>, and Github credentials in <code>~/.github</code> - this duplication is because the two 
  * git-related libraries get their configuration from different places.
+ * 
+ * The keys you need to supply values for in <code>~/.gradle/gradle.properties</code> are:
+ * <ul>
+ *     <li>sonatypeUsername</li>
+ *     <li>sonatypePassword</li>
+ *     <li>signing.keyId</li>
+ *     <li>signing.secretKeyRingFile</li>
+ *     <li>signing.password</li>
+ *     <li>github.credentials.username</li>
+ *     <li>github.credentials.password</li>
+ * </ul>
  */
 class ReleasePlugin implements Plugin<Project> {
     private Project project
@@ -27,83 +37,14 @@ class ReleasePlugin implements Plugin<Project> {
         project.evaluationDependsOnChildren()
 
         project.task('prepareRelease', type: PrepareReleaseTask, dependsOn: project.subprojects.clean)
-//        magicIncantationRequiredToGetNexusPluginWorkingOnSubprojects()
+        // uploadArchives is currently configured in maven-deployment.gradle
+        project.task('release', dependsOn: ['prepareRelease', project.subprojects.uploadArchives])
+
 //        project.task('draftReleaseNotes', type: DraftReleaseNotesTask, dependsOn: [project.subprojects.uploadArchives])
 //        project.task('publishJavadoc', type: PublishJavadocTask, dependsOn: ['draftReleaseNotes', project.subprojects.javadoc])
 //        project.task('updateToNextVersion', type: UpdateToNextVersionTask, dependsOn: 'publishJavadoc')
 //        project.task('release', dependsOn: 'updateToNextVersion')
     }
 
-    def magicIncantationRequiredToGetNexusPluginWorkingOnSubprojects() {
-        // feels like most of this shouldn't be needed as the repos etc are defaults, but they are required when you 
-        // add this as a dependency to a top-level project instead of just running it alone
-        project.subprojects { subproject ->
-            apply plugin: 'nexus'
-
-            project.ext.mavenDeployers = []
-
-            def pom = { config ->
-                project.mavenDeployers*.pom config
-            }
-
-            install {
-                project.mavenDeployers << repositories.mavenInstaller
-            }
-
-            uploadArchives {
-                project.mavenDeployers << repositories.mavenDeployer {
-                    def nexusCredentials = [
-                            userName: project.properties.nexusUsername,
-                            password: project.properties.nexusPassword
-                    ]
-
-                    snapshotRepository(url: 'https://oss.sonatype.org/content/repositories/snapshots') {
-                        authentication(nexusCredentials)
-                    }
-                    repository(url: 'https://oss.sonatype.org/service/local/staging/deploy/maven2/') {
-                        authentication(nexusCredentials)
-                    }
-                }
-                repositories {
-                    mavenDeployer {
-                        beforeDeployment { MavenDeployment deployment -> signing.signPom(deployment) }
-                    }
-                }
-            }.mustRunAfter(this.project.prepareRelease)
-
-            modifyPom {
-                project {
-                    name = 'Morphia'
-                    description = 'Java Object Document Mapper for MongoDB'
-                    url 'https://github.com/mongodb/morphia.git'
-
-                    scm {
-                        url 'https://github.com/mongodb/morphia.git'
-                        connection 'scm:git:https://github.com/mongodb/morphia.git'
-                    }
-
-                    licenses {
-                        license {
-                            name 'The Apache Software License, Version 2.0'
-                            url 'http://www.apache.org/licenses/LICENSE-2.0.txt'
-                            distribution 'repo'
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            name 'Various'
-                            organization = 'MongoDB'
-                        }
-                    }
-                }
-
-                whenConfigured { resultPom ->
-                    resultPom.dependencies.removeAll { dep -> dep.scope != 'compile' }
-                    resultPom.dependencies*.scope = null
-                }
-            }
-        }
-    }
 }
 
